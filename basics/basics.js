70f2ef53c1fb5d55db8a3ff3cce7b0537e1f0219
manikanta <manikantatez@gmail.com>
2024-06-26 20:19:19 +0200

    basics (numbers)

---
+++ basics/basics.js
@@ -0,0 +1,13 @@
+/*
+In a computer, everything is data. You can read, modify, and create data,
+but nothing else exists.
+All data is stored as sequences of bits, making it fundamentally similar.
+13 --> 1101 (4bits)(1/2 byte)
+
+!Data Types
+
+!Numbers
+Java script uses 64 bits to store a single number
+it alocates that bits to store a number .
+even the number is smaller like 0 or larger like 10^10 it uses complete 64 bits
+*/
