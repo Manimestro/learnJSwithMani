5afefb6e55936440eaece91df914a603e5154c4a
manikanta <manikantatez@gmail.com>
2024-06-27 20:19:19 +0200

    basics (Arithmetic)

--- basics/basics.js
+++ basics/basics.js
@@ -10,4 +10,11 @@ All data is stored as sequences of bits, making it fundamentally similar.
 Java script uses 64 bits to store a single number
 it alocates that bits to store a number .
 even the number is smaller like 0 or larger like 10^10 it uses complete 64 bits
+
+!Arithmetic
+Operators have precedence, determining the order in which operations are performed.
+*,%,/ --> takes presendce over +,-
+Parentheses can be used to explicitly define the desired order of operations.
+
+
 */
