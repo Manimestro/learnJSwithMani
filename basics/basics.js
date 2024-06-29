89a0c259a3df8c3a8488270dcbe29bc312fa550d
manikanta <manikantatez@gmail.com>
2024-06-29 20:19:19 +0200

    basics (strings)

--- basics/basics.js
+++ basics/basics.js
@@ -7,9 +7,9 @@ All data is stored as sequences of bits, making it fundamentally similar.
 !Data Types
 
 !Numbers
-Java script uses 64 bits to store a single number
+Java script uses 32 bits to store a single number
 it alocates that bits to store a number .
-even the number is smaller like 0 or larger like 10^10 it uses complete 64 bits
+it varies on small, long and longer
 
 !Special Numbers
 Infinity , -Infinity , NaN are number types
@@ -18,6 +18,10 @@ Infinity , -Infinity , NaN are number types
 Operators have precedence, determining the order in which operations are performed.
 *,%,/ --> takes presendce over +,-
 Parentheses can be used to explicitly define the desired order of operations.
+
+!Strings
+can be double,single, and backticks
+for each char it uses --> 6bits
 
 
 */
