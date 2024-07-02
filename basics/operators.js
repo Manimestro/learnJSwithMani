ec0bf9acc715f4b6af3fbb33760b023cb1060ad1
manikanta <manikantatez@gmail.com>
2024-07-02 20:19:19 +0200

    Comparison Operators,String Comparison

---
+++ basics/operators.js
@@ -0,0 +1,11 @@
+/*
+!Comparison Operators
+Comparison operators in JavaScript are used to compare values and produce Boolean values (true or false)
+
+!String Comparison
+When comparing strings, JavaScript uses Unicode values and compares them character by character from left to right
+console.log("Aardvark" < "Zoroaster"); // → true
+Uppercase letters are always "less" than lowercase letters
+
+console.log(NaN == NaN); // → false
+*/
