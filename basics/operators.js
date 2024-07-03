d1d4d4316796061333709a198ce4b79b6f6d033d
manikanta <manikantatez@gmail.com>
2024-07-03 20:19:19 +0200

    Logical Operators

--- basics/operators.js
+++ basics/operators.js
@@ -8,4 +8,10 @@ console.log("Aardvark" < "Zoroaster"); // → true
 Uppercase letters are always "less" than lowercase letters
 
 console.log(NaN == NaN); // → false
+
+!Logical Operators (&& || !)
+Ternery operators 
+console.log(true ? 1 : 2);  // → 1
+console.log(false ? 1 : 2); // → 2
+
 */
