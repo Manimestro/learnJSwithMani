68cd396148492f553ceb88f912fe80d48d7bea55
manikanta <manikantatez@gmail.com>
2024-07-27 20:19:19 +0200

    Statement vs Expression

---
+++ basics/program-structure.js
@@ -0,0 +1,11 @@
+/*
+! Statement vs Expression
+
+Expression --> A piece of code that evaluates to a value.
+ex: 2+3
+Statement --> A complete instruction in JavaScript, typically consisting of expressions
+ex: console.log(2+3)
+
+
+
+*/ 
