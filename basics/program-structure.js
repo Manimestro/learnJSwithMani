ff51dfaff0dd0bdef5cf07f8c65ba019ff573f30
manikanta <manikantatez@gmail.com>
2024-07-27 20:19:19 +0200

    Environment

--- basics/program-structure.js
+++ basics/program-structure.js
@@ -6,7 +6,7 @@ ex: 2+3
 Statement --> A complete instruction in JavaScript, typically consisting of expressions
 ex: console.log(2+3)
 
-!Bindings (variables)
+! Bindings (variables)
 
 these are refernces that stores and manupulates data over a time in a programe
 let,var,const --> with these we can defined binding 
@@ -16,6 +16,10 @@ const --> cant re-assinged after declaration
 
 dont use keywords,reserved words for binding names 
 
+! Envinorments 
+This is a group of bindings that are avaible to a programe in that context 
+there are some initial bindings present in the environment (Node or browser)
+like keywords, window for browser , system defaults etc
 
 
 */ 
