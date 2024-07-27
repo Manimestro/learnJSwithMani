f71418ecbf40afc17a3a46f7601015dd7ada12c2
manikanta <manikantatez@gmail.com>
2024-07-27 20:19:19 +0200

    Bindings

--- basics/program-structure.js
+++ basics/program-structure.js
@@ -6,6 +6,16 @@ ex: 2+3
 Statement --> A complete instruction in JavaScript, typically consisting of expressions
 ex: console.log(2+3)
 
+!Bindings (variables)
+
+these are refernces that stores and manupulates data over a time in a programe
+let,var,const --> with these we can defined binding 
+let --> can be re-assigned after declaration 
+var --> can be re declare (used in 2015 ecmascript)
+const --> cant re-assinged after declaration
+
+dont use keywords,reserved words for binding names 
+
 
 
 */ 
