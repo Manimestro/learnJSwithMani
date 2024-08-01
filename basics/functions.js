61b5ad13badb155a24006f99c296a447813bfa00
manikanta <manikantatez@gmail.com>
2024-08-01 20:19:19 +0200

    Functions as values

--- basics/functions.js
+++ basics/functions.js
@@ -17,3 +17,15 @@ function local(){
     let bindF = 3 // local scoped (local to function)
 }
 // bindings defined with var has no block scope barrier 
+/*
+! nested scope
+
+so local scopes can see the outer scopes which the local scope is used and also the global scope 
+but global scope cannot see the local scope 
+
+! Functions as values 
+
+function binding is just a name and value is a programe we can re-assign the binding 
+and we can pass it to another function binding as variable 
+
+*/
