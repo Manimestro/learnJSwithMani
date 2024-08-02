0a78f13a1db99a353364fea697fb99e5b74711c1
manikanta <manikantatez@gmail.com>
2024-08-02 20:19:19 +0200

    methods,Objects

--- basics/objects_arrays.js
+++ basics/objects_arrays.js
@@ -4,7 +4,7 @@
 ! Arrays
 [1,2,3] --> holds data sets a set of values 
 */
-//properties --> bindings have properties, that we can define
+// !properties --> objects have properties, that we can define
 var a = "string";
 var arr = [1,2,3];
 a.length // calling length property via .
@@ -13,4 +13,32 @@ arr.length
 arr["length"]
 // to get the values from the array we need to call the index number as property name 
 // arr.2 --> we cant call non binding names with . 
-arr[2] //--> so we call with square braces 
+arr[2] //--> so we call with square braces
+
+// !Methods
+// if the property of is a function then it is called method 
+const names="Jhon"
+names.toLocaleLowerCase() // it is a method of string 
+
+// !Objects
+const obj = {
+    name:"joe",
+    age:22
+}
+const obj2 = {
+    name:"mani",
+    age:22
+}
+console.log(obj.dob) //undefined 
+obj.dob = "20-03-2002" // assigning properties
+delete obj.dob //deleting properties 
+
+Object.keys(obj) // all keys
+Object.assign(obj,obj2) // merge two objs
+
+let arr2 = [1,2,3]
+typeof arr2
+console.log(Object.keys(arr2))
+console.log(Object.values(arr2))
+console.log(arr2["1"])
+// arrays are also objects with keys as indexes
