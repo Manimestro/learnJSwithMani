1cc07b43730fb10ca6e6d769c62adeeef4ee90d3
manikanta <manikantatez@gmail.com>
2024-08-06 20:19:19 +0200

    Array, properties

---
+++ basics/objects_arrays.js
@@ -0,0 +1,16 @@
+/*
+! Data Structures
+
+! Arrays
+[1,2,3] --> holds data sets a set of values 
+*/
+//properties --> bindings have properties, that we can define
+var a = "string";
+var arr = [1,2,3];
+a.length // calling length property via .
+a["length"] // calling length property via []
+arr.length
+arr["length"]
+// to get the values from the array we need to call the index number as property name 
+// arr.2 --> we cant call non binding names with . 
+arr[2] //--> so we call with square braces 
