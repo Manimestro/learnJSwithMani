220305e0fd1667e6716be0a01b8fcfe98b1f66df
manikanta <manikantatez@gmail.com>
2024-08-15 11:57:45 +0530

    Object Oriented programming

---
+++ basics/objects.js
@@ -0,0 +1,10 @@
+/*
+! Object Oriented programming (oops)
+object- oriented programming has shaped the design of many programming languages, including JavaScript
+
+it is a set of techniques for  designing a program using objects
+
+Abstract data types
+strings, numbers, arrays these all are object classes where it exposes some properties to use
+
+*/
