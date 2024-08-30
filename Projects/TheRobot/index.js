3e099de7da8111766918c7f2f37839a679d3e3b9
manikanta <manikantatez@gmail.com>
2024-08-30 20:19:19 +0200

    Project-Robot: VillageState, immutableObjects(freeze)

--- Projects/TheRobot/index.js
+++ Projects/TheRobot/index.js
@@ -33,4 +33,49 @@ function buildGraph(edges) {
 
 const roadGraph = buildGraph(roads);
 console.log(roadGraph)
+/* 
+ When thinking in Opp's dont create everything you see as class Objects and relate it with others
+ Reflexively writing classes for every concept in your application tends to leave you with a collection of interconnected 
+ objects that each have their own internal, changing state. Such programs are often hard to understand and thus easy to break.
+*/
 
+//VillageState holds the info of the currect place the robot in and parcels it holds 
+class VillageState{
+    constructor(place, parcels){
+        this.place = place
+        this.parcels = parcels
+    }
+    move(destination){
+        if(!roadGraph[this.place].includes(destination)){
+            return this
+        }
+        else{
+            let parcels = this.parcels.map(p=>{
+                if(p.place != this.place) return p 
+                return {place:destination,address:p.address}
+            }).filter(p=>p.place != p.address)
+            return new VillageState(destination,parcels)
+        }
+    }
+}
+
+let first = new VillageState(
+    "Post Office",
+    [{place:"Post Office",address:"Alice's House"}]
+)
+let next = first.move("Alice's House")
+
+console.log(next.place)
+
+console.log(next.parcels)
+
+console.log(first.place)
+
+// Persistent Data (data structures that wont change over time)
+
+let object = Object.freeze({value: 5});
+ object.value = 10;
+ console.log(object.value);
+ // → 5
+ 
+ 
