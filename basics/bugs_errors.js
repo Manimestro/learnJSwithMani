093f480752fcc0472253f8dbe05bc849174d5fc8
manikanta <manikantatez@gmail.com>
2024-09-02 20:19:19 +0200

    Bugs- Language

---
+++ basics/bugs_errors.js
@@ -0,0 +1,8 @@
+//! Bugs and Errors 
+
+//! Language 
+
+// javascript complains if we miss the grammer of langague 
+// and so things like calling a non function will raise errors on code exicution 
+// somethings dont even raise errors it will pass and make output wrong 
+// The process of finding mistakes in programs is called debugging.
