5b6ac59161e298ca5b2b5278c242e1369643f6ce
manikanta <manikantatez@gmail.com>
2024-09-03 20:19:19 +0200

    Bugs- UseStrict

--- basics/bugs_errors.js
+++ basics/bugs_errors.js
@@ -1,8 +1,28 @@
 //! Bugs and Errors 
 
-//! Language 
+//* Language 
 
 // javascript complains if we miss the grammer of langague 
 // and so things like calling a non function will raise errors on code exicution 
 // somethings dont even raise errors it will pass and make output wrong 
 // The process of finding mistakes in programs is called debugging.
+
+//* UseStrict
+
+function canYouSpotTheProblem() {
+    // 'use strict'
+     a = 10
+    }
+canYouSpotTheProblem()
+/*
+    The use strict will strictly check for more errors and complain on exicution
+    The classes and modules are enabled by default
+ */
+
+function Person(name){
+    // "use strict"
+    this.name = name
+}
+let man = Person("Mani")
+console.log(name)
+// using strict mode in functions the this context will become undefined if the function not called correctly
