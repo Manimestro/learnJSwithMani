f85df8bf0593ba936d433ba236ddfb09623a02bd
Manikanta Prasad Lopinti <manikantatez@gmail.com>
2024-09-19 12:30:00 +0530

    Modules

---
+++ basics/modules.js
@@ -0,0 +1,9 @@
+//! Modules 
+/**
+    Big ball of mud --> if a programe is not well structured and can interact and change anything in the code 
+    This will become a big mess, to writen the code well modules are introduced 
+    where the programe will give an interface what it can offer and explicetly tells what 
+    modules it depends in order to run 
+ */
+
+    
