a12e699e513ebb9c87b7a6634d15eadeb344a928
Manikanta Prasad Lopinti <manikantatez@gmail.com>
2024-09-20 12:30:00 +0530

    Modules and Scripts

--- basics/modules.js
+++ basics/modules.js
@@ -1,9 +1,22 @@
 //! Modules 
-/**
+/*
     Big ball of mud --> if a programe is not well structured and can interact and change anything in the code 
     This will become a big mess, to writen the code well modules are introduced 
     where the programe will give an interface what it can offer and explicetly tells what 
     modules it depends in order to run 
  */
 
-    
+/*
+*Modules and Scripts 
+After 2015 ecma script build modules are introduced 
+before script used to create a global binding and referenced that name in other script (that is only interaction of two scripts)
+when ES modules cames with import and export statements we can use that to do import and export of modules with file paths and different platforms resolve it 
+differently and import them 
+*/
+export const Variable = 1
+const Variable2 = 1
+export function xyz(){
+
+}
+export default Variable2
+
