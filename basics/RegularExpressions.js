5b3ba2e152bb49e3bd5ca6bdc992710d1658bdee
Manikanta Prasad Lopinti <manikantatez@gmail.com>
2024-09-24 09:00:00 +0530

    Repeating parts of patterns

--- basics/RegularExpressions.js
+++ basics/RegularExpressions.js
@@ -12,7 +12,7 @@ console.log(manualRegex.test('abcd')) // --> true
 
 // to find wheter a digit exisits in string 
 
-//* Sets of characters
+//* Sets of characters []
 let digitcheck = /[0123456789]/
 digitcheck = /[0-9]/
 digitcheck = /\d/
@@ -25,6 +25,30 @@ let datetimeregex = /\d\d-\d\d-\d\d\d\d \d\d:\d\d[AP]M/
 console.log(datetimeregex.test(datetime), "datetime test")
 
 let nondDigitsRegex = /[^\d]/
-let person = "mani"
+let person = "mani1"
 
 console.log("🚀 ~ nondDigitsRegex:", nondDigitsRegex.test(person))
+
+//* Repeating parts of patterns
+
+// gorup of digits if exists in a string 
+let groupofDigits = /\d*/
+let digits = "s"
+console.log("🚀 ~ groupofDigits:", groupofDigits.test(digits)) //true
+
+// gorup of digits even no exists 
+// A* A is optional 
+groupofDigits = /\d*/
+digits = "abc"
+console.log("🚀 ~ groupofDigits:", groupofDigits.test(digits)) //true
+
+//optional part of pattern ?
+// name? e is optional
+let optionalRegex = /name?/
+console.log("🚀 ~ optionalRegex:", optionalRegex.test('name'))
+console.log("🚀 ~ optionalRegex:", optionalRegex.test('nam'))
+
+// A{1,2} A should come at least once at max twice 
+datetimeregex = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{1,2}/
+console.log("🚀 ~ datetimeregex:", datetimeregex.test("1-30-2003 8:45"))
+
