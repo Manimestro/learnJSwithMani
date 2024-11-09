6c444adfd9af2a774b16d15d8280bc40fc488678
Manikanta Prasad Lopinti <manikantatez@gmail.com>
2024-11-09 12:30:00 +0530

    Asynchronicity

---
+++ basics/asynchronousProgramming.js
@@ -0,0 +1,6 @@
+//! Asynchronicity
+/*
+    Running the  pragrame while not waiting for I/O operations like reading from disk and network requests 
+    while the I/O operation is done parlelly still the programe is able to run instead of waiting 
+    waiting for actions to finish is implicit in the synchronous model, while it is explicit—under our control—in the asynchronous one
+*/
